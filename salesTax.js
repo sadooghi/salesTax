9b3bde8324212d17a721419fc4e4fa9635970415
sadooghi <gh.sadooghi@gmail.com>
2017-03-08 23:40:12 +0000

    sales tax calculator

---
+++ salesTax.js
@@ -0,0 +1,69 @@
+var salesTaxRates = {
+  AB: 0.05,
+  BC: 0.12,
+  SK: 0.10
+};
+
+var companySalesData = [
+  {
+    name: "Telus",
+    province: "BC",
+    sales: [ 100, 200, 400 ]
+  },
+  {
+    name: "Bombardier",
+    province: "AB",
+    sales: [ 80, 20, 10, 100, 90, 500 ]
+  },
+  {
+    name: "Telus",
+    province: "SK",
+    sales: [ 500, 100 ]
+  }
+];
+
+
+// function totalSales(salesData){
+//   var salesSum = 0;
+//   for (var i = 0; i < salesData.length; i++){
+//     for (var j = 0; j < sales.length; j++){
+//       salesData[i].sales[j] += salesSum;
+//     }
+//   }
+//   return salesSum;
+// }
+
+
+function totalTax(salesData, taxRates) {
+  var taxRate = taxRates[salesData.province];
+  var taxAmount = taxRate*totalSales(salesData.sales);
+  console.log(taxRate);
+  return taxAmount;
+}
+
+function totalSales(sales) {
+  var salesSum = 0;
+  for (var j = 0; j < sales.length; j++){
+    salesSum += sales[j];
+  }
+  return salesSum;
+}
+
+function calculateSalesTax(salesData, taxRates) {
+  // Implement your code here
+  var result= {};
+
+  for ( var i = 0; i < salesData.length; i++){
+    if (result[salesData[i].name] === undefined ){
+      result[salesData[i].name] = {totalSales: totalSales( salesData[i].sales ) , totalTaxes: totalTax(salesData[i], taxRates)};
+    } else {
+      result[salesData[i].name].totalSales += totalSales(salesData[i].sales);
+      result[salesData[i].name].totalTaxes += totalTax(salesData[i], taxRates);
+    }
+    
+  }
+  return result;
+}
+
+var results = calculateSalesTax(companySalesData, salesTaxRates);
+console.log(results);
